61b83e02662180d8610c62b33275ca9443fd46e2
dhruvalk <kothari.dhruval@gmail.com>
2024-02-24 22:03:05 +0800

    progress on extension main page

--- Extension/script.js
+++ Extension/script.js
@@ -1,1 +1,19 @@
 let durationDisplay = document.getElementById("duration");
+let reduceDurationButton = document.getElementById("reduce-duration");
+let increaseDurationButton = document.getElementById("increase-duration");
+let trackAnotherButton = document.getElementById("track-another");
+
+function increaseDuration() {
+  durationDisplay.textContent =
+    (parseInt(durationDisplay.textContent.split(" ")[0]) + 5).toString() +
+    " mins";
+}
+
+function reduceDuration() {
+  durationDisplay.textContent =
+    (parseInt(durationDisplay.textContent.split(" ")[0]) - 5).toString() +
+    " mins";
+}
+
+reduceDurationButton.addEventListener("click", reduceDuration);
+increaseDurationButton.addEventListener("click", increaseDuration);
