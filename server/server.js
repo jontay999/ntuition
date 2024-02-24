24cc838327ea8a04a0c281506234cd1e0e0cb614
Jonathan Tay <jontay999@gmail.com>
2024-02-24 20:33:15 +0800

    basic flow set up

--- server/server.js
+++ server/server.js
@@ -12,20 +12,25 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server);
 
-// code : client
-const handshakes = {
+// code : child_socket id
+const codes = {
 
 }
 
 // child : parent
-const connections = {
+const child_to_parent_connections = {
+
+}
+
+// parent: child
+const parent_to_child_connections = {
 
 }
 const random_code = () => {
     return "123456"
     while (true) {
         const num = (Math.floor(100000 + Math.random() * 900000)).toString();
-        if (!(num in handshakes)) {
+        if (!(num in codes)) {
             return num;
         }
     }
@@ -47,19 +52,70 @@ app.get('/parent', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    // CHILD SAYS IT WANTS TO BE MONITORED
-    socket.on("monitor_request", (data) => {
-        console.log("child monitor request-> ", data)
+    // CHILD SAYS IT WANTS TO BE MONITORED , data = monitor_details in JSON string
+    socket.on("child_monitor_request", () => {
+        console.log("child:", socket.id, "wants to be monitored")
         const code = random_code();
-        console.log("Generated code:", code)
-        socket.emit('code', code);
+        codes[code] = socket.id
+        socket.emit('child_code', code);
     })
 
-    // child sends
-    socket.on("monitor_details", (data) => {
-        const json_data = JSON.parse(data);
-        console.log("got json_data")
+    // PARENT KEYS IN CODE
+    socket.on("parent_accept_request", (code) => {
+        console.log("parent enters in code", code)
+
+        if (!(code in codes)) {
+            console.error("GOT A CODE THAT HAD NO CLIENT!");
+            return;
+        }
+        const child_id = codes[code];
+
+        // set up child to parent connection preemptively
+        child_to_parent_connections[child_id] = socket.id;
+
+        // delete code
+        delete codes[code];
+
+        // ask client to confirm handshake with parent
+        io.to(child_id).emit('child_complete_handshake', socket.id);
     })
+
+    // child confirms handshake
+    socket.on("child_confirm_handshake", (parent_id) => {
+        console.log("child confirms handshake, all good to go!")
+
+        // make sure connections are good
+        parent_to_child_connections[parent_id] = socket.id;
+        if (parent_id !== child_to_parent_connections[socket.id]) {
+            console.error("messed up alr kenna", parent_id, child_to_parent_connections[socket.id])
+        }
+
+        // tell parent that connection is set up
+        io.to(parent_id).emit("parent_begin_monitoring", socket.id)
+
+    })
+
+
+    // child visit blacklisted site
+    socket.on("child_is_bad_boy", site => {
+        console.log("child was v bad and visited:", site)
+        const parent_id = child_to_parent_connections[socket.id]
+        console.log("child parent is:", parent_id)
+
+        // complain to parent
+        io.to(parent_id).emit("parent_kenna_complain", site)
+    })
+
+    // parent send alert to child
+    socket.on("parent_call_police", scolding => {
+        console.log("parent want call police alr:", scolding)
+
+        const child_id = parent_to_child_connections[socket.id];
+        io.to(child_id).emit("child_kenna_scolded", scolding);
+    })
+
+
+
 });
 
 
