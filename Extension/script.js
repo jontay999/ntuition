83d839d72d0e662002949eeede6a39e6af941194
NABEEL004 <66759134+NABEEL004@users.noreply.github.com>
2024-02-24 23:28:32 +0800

    completed timer page

--- Extension/script.js
+++ Extension/script.js
@@ -26,6 +26,44 @@ function reduceDuration() {
 reduceDurationButton.addEventListener("click", reduceDuration);
 increaseDurationButton.addEventListener("click", increaseDuration);
 
+function addWebsite() {
+    // Get the input value
+    var newWebsiteInput = document.getElementById("newWebsite");
+    var newWebsite = newWebsiteInput.value.trim();
+  
+    // Check if the input is not empty
+    if (newWebsite !== "") {
+      // Create a new checkbox and label
+      var newCheckbox = document.createElement("input");
+      newCheckbox.type = "checkbox";
+      newCheckbox.id = newWebsite.toLowerCase();
+      newCheckbox.name = newWebsite.toLowerCase();
+      newCheckbox.checked = true;
+  
+      var newLabel = document.createElement("label");
+      newLabel.htmlFor = newWebsite.toLowerCase();
+      newLabel.textContent = newWebsite;
+  
+      // Create a new div for the entry
+      var newEntry = document.createElement("div");
+      newEntry.className = "flex items-center gap-3 ml-4 my-1";
+      newEntry.appendChild(newCheckbox);
+      newEntry.appendChild(newLabel);
+  
+      // Append the new entry to the blacklist section
+      var blacklistSection = document.getElementById("blacklist-section");
+      blacklistSection.insertBefore(
+        newEntry,
+        document.getElementById("add-website")
+      );
+  
+      // Clear the input field
+      newWebsiteInput.value = "";
+    }
+  }
+  
+  addNewWebsiteButton.addEventListener("click", addWebsite);
+
 // index2.html script
 
 document.getElementById("copy").addEventListener("click", function () {
@@ -51,10 +89,6 @@ document.getElementById("codeInput").addEventListener("input", function (e) {
   this.value = this.value.replace(/\D/g, "");
 });
 
-function addWebsite() {
-  // Get the input value
-  var newWebsiteInput = document.getElementById("newWebsite");
-  var newWebsite = newWebsiteInput.value.trim();
 
   // Check if the input is not empty
   if (newWebsite !== "") {
@@ -76,7 +110,7 @@ function addWebsite() {
     newEntry.appendChild(newLabel);
 
     // Append the new entry to the blacklist section
-    var blacklistSection = document.getElementById("blacklist-section");
+    let blacklistSection = document.getElementById("blacklist-section");
     blacklistSection.insertBefore(
       newEntry,
       document.getElementById("add-website")
@@ -85,7 +119,7 @@ function addWebsite() {
     // Clear the input field
     newWebsiteInput.value = "";
   }
-}
+
 
 function generateCode() {
   var blacklistSites = [];
